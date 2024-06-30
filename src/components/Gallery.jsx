80bb00ace56c1f561a00c3100b0f9fae961ed203
PrasanPoudel <prasanpoudel501@gmail.com>
2024-06-30 22:29:14 +0545

    try fixing bug

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -10,24 +10,24 @@ const Gallery = () => {
           <div className="flex flex-wrap md:-m-2 -m-1">
             <div className="flex flex-wrap w-1/2">
               <div className="md:p-2 p-1 w-1/2">
-                <img alt="gallery" className="w-full object-cover h-full object-center block" src="./image/gallary_1.jpg"/>
+                <img alt="gallery" className="w-full object-cover h-full object-center block" src="/image/gallary_1.jpg"/>
               </div>
               <div className="md:p-2 p-1 w-1/2">
-                <img alt="gallery" className="w-full object-cover h-full object-center block" src="./image/gallary_2.jpg"/>
+                <img alt="gallery" className="w-full object-cover h-full object-center block" src="/image/gallary_2.jpg"/>
               </div>
               <div className="md:p-2 p-1 w-full">
-                <img alt="gallery" className="w-full h-full object-cover object-center block" src="./image/gallary_3.jpg"/>
+                <img alt="gallery" className="w-full h-full object-cover object-center block" src="/image/gallary_3.jpg"/>
               </div>
             </div>
             <div className="flex flex-wrap w-1/2">
               <div className="md:p-2 p-1 w-full">
-                <img alt="gallery" className="w-full h-full object-cover object-center block" src="./image/gallary_4.jpg"/>
+                <img alt="gallery" className="w-full h-full object-cover object-center block" src="/image/gallary_4.jpg"/>
               </div>
               <div className="md:p-2 p-1 w-1/2">
-                <img alt="gallery" className="w-full object-cover h-full object-center block" src="./image/gallary_5.jpg"/>
+                <img alt="gallery" className="w-full object-cover h-full object-center block" src="/image/gallary_5.jpg"/>
               </div>
               <div className="md:p-2 p-1 w-1/2">
-                <img alt="gallery" className="w-full object-cover h-full object-center block" src="./image/gallary_6.jpg"/>
+                <img alt="gallery" className="w-full object-cover h-full object-center block" src="/image/gallary_6.jpg"/>
               </div>
             </div>
           </div>
